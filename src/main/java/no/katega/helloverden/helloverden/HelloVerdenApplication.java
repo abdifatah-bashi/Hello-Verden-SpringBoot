34690540092de762f5abdb4630016f5cf863c588
Abdifatah A. Bashi <fitah22@gmail.com>
2018-09-12 22:08:46 +0200

    First commit

---
+++ src/main/java/no/katega/helloverden/helloverden/HelloVerdenApplication.java
@@ -0,0 +1,12 @@
+package no.katega.helloverden.helloverden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloVerdenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloVerdenApplication.class, args);
+	}
+}
